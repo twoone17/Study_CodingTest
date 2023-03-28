7ecb20ffb9192637e714e500d56c5604e6ef0f2c
euisung <ghddmltjd11@gmail.com>
2023-03-29 00:06:48 +0900

    3/28 자료구조 +4

--- javascript /7)알고리즘 기초 1/1)자료구조 1/3. 스택수열 1874.js
+++ javascript /7)알고리즘 기초 1/1)자료구조 1/3. 스택수열 1874.js
@@ -1,24 +1,31 @@
 const fs = require('fs');
-const input = fs.readFileSync('../../test.txt').toString().split('\n').map(item =>+item)
+// const input = fs.readFileSync('../../test.txt').toString().split('\n').map(item => +item)
+const input = fs.readFileSync('dev/stdin').toString().split('\n').map(item => +item)
 const n = input.shift();
 const array = [];
-let num;
 let count = 1;
 let result = ''
-for(let i =0 ;i<n;i++)
-{
-    num = input.shift()
-    while(count < num)
-    {
-        count++
-        array.push(count)
-        result += '+'
-    }
-    if(count === num)
-    {
-        array.pop()
+let temp = n + 1;
+
+function solution() {
+    for (let i = 0; i < n; i++) {
+        let num = input.shift()
+        while (count <= num) {
+            array.push(count++)
+            result += '+'
+            result += '\n'
+        }
+
+            const poppedNum = array.pop();
+        if(poppedNum !=num)
+        {
+            return 'NO'
+        }
+
         result += '-'
+        result += '\n'
     }
-
-
+    return result
 }
+
+console.log(solution())
