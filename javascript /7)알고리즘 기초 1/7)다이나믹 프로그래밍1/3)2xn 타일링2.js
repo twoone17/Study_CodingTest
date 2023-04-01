89b6ea3e6fadf874d590ef033a68c275e5fe5584
euisung <ghddmltjd11@gmail.com>
2023-04-02 00:46:45 +0900

    4/1 dp +3

---
+++ javascript /7)알고리즘 기초 1/7)다이나믹 프로그래밍1/3)2xn 타일링2.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+// const input = fs.readFileSync('../../test.txt').toString()
+const input = fs.readFileSync('dev/stdin').toString().trim()
+const n = parseInt(input)
+let dp = Array(n + 1).fill(0);
+// let dp = []
+dp[1] = 1;
+dp[2] = 3;
+function solution(n) {
+
+
+    for (let i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1] + 2*dp[i - 2])%10007
+    }
+
+    return dp[n]
+}
+
+console.log(solution(n));
+
+//왜 자꾸 에러뜨냐 ㅅㅂ
