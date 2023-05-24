86c69647cad464fa8839a3f89dcddecacf21c3fa
euisung <ghddmltjd11@gmail.com>
2023-05-24 20:47:32 +0900

    5/24

--- javascript /7)알고리즘 기초 1/7)다이나믹 프로그래밍1/11)가장 긴 증가하는 수열 4 14002.js
+++ javascript /7)알고리즘 기초 1/7)다이나믹 프로그래밍1/11)가장 긴 증가하는 수열 4 14002.js
@@ -3,8 +3,9 @@ let input = fs.readFileSync('../../test.txt').toString().trim().split('\n')
 // let input = fs.readFileSync('dev/stdin').toString().trim().split('\n')
 const N = Number(input.shift())
 input = input.shift().split(' ').map(item=>+item)
-let dp = [[1,[input[0]]]]
-console.log(dp)
+let dp = Array.from(Array(N),()=> new Array(2))
+// dp = [[1,[input[0]]]]
+dp[0] = [1,input[0]]
 
 let max
 let array = []
@@ -17,10 +18,13 @@ for(let i = 1; i<N;i++)
         if(input[i]>input[j])
         {
            max = Math.max(max,dp[j][0])
-            array = [...array,input[j]]
+            // array = [...array,input[j]]
+            array.push(input[j])
+            console.log(input[j])
+            console.log(array)
         }
     }
-    console.log(dp)
+    // console.log(dp)
     dp[i][0] = max +1
     dp[i][1] = [...array]
 }
